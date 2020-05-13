b6a73bee68a9c1afb3eab6103d1a1cea904d9ffe
mishael254 <mishaelmishantoz@gmail.com>
2020-05-13 15:47:58 +0300

    added a div id of output to output the result below the form in both index.html file and scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -16,21 +16,16 @@ return day&&akanMaleNames[day.getDay()];
 $(document).ready(function() {
     $("#blanks form").submit(function(event) {
         var person1Input=$("input#person1").val();
-
-
         $(".person1").text(person1Input);
-
-
+        var gender = $("input:radio[name=gender]:checked").val()
+        var dob = $("#born").val();
+        $(".born").text(dob);
+        var result=akan(dob,gender);
+        $("#output").text(result);
         $("#description").show();
 
+
         event.preventDefault();
-        var gender = $("input:radio[name=flavor]:checked").val()
-        var dob = $("#born").val();
 
-
-
-
-
-        $("output").text(result);
       });
     });
